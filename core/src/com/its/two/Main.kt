ce351698fc33ce7c7f8d2a37adc087ce391dc49e
Abhishek Aryan <itsabhiaryan@gmail.com>
2017-05-18 03:08:47 +0530

    box2d integration.

--- core/src/com/its/two/Main.kt
+++ core/src/com/its/two/Main.kt
@@ -3,15 +3,20 @@ package com.its.two
 import com.badlogic.gdx.Application
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.assets.AssetDescriptor
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.assets.loaders.resolvers.ExternalFileHandleResolver
 import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.physics.box2d.*
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Button
 import com.badlogic.gdx.scenes.scene2d.ui.Container
@@ -22,19 +27,85 @@ import java.util.logging.FileHandler
  * Created by Abhishek Aryan on 11-06-2016.
  */
 
-class Main : ApplicationAdapter() {
+class Main : ApplicationAdapter(),InputProcessor {
+    override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
+    }
+
+    override fun mouseMoved(screenX: Int, screenY: Int): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return false
+    }
+
+    override fun keyTyped(character: Char): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun scrolled(amount: Int): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun keyUp(keycode: Int): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun keyDown(keycode: Int): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+
+        vec3.set(screenX.toFloat(),screenY.toFloat(),0F)
+
+        var cor=cam.unproject(vec3)
+
+        var bodydef=BodyDef()
+        bodydef.position.set(cor.x,cor.y)
+        bodydef.type=BodyDef.BodyType.DynamicBody
+
+        var poly=PolygonShape()
+        poly.setAsBox(20F,5F)
+
+
+        var fixturedef=FixtureDef()
+        fixturedef.shape=poly
+
+        var body=world.createBody(bodydef)
+        body.createFixture(fixturedef)
+
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     internal lateinit  var batch: SpriteBatch
     internal lateinit var img: Texture
 
     internal lateinit var stage: Stage
+    internal lateinit var world: World
+    internal lateinit var b2dRenderer:Box2DDebugRenderer
+    internal lateinit var cam:OrthographicCamera
 
-
+    internal lateinit var vec3:Vector3
 
     override fun create() {
         batch = SpriteBatch()
         img = Texture("badlogic.jpg")
         var tex = TextureRegionDrawable(TextureRegion(img))
+
+
+        world= World(Vector2(0F,-9.8F),true)
+        world.setContactListener(MyContactListener())
+        b2dRenderer= Box2DDebugRenderer();
+        cam= OrthographicCamera();
+        cam.setToOrtho(false,48F,80F)
+
+        vec3= Vector3()
+
+
 
 
 //        if(Gdx.app.type==Application.ApplicationType.Android) {
@@ -55,25 +126,31 @@ class Main : ApplicationAdapter() {
 //        }
 
 
+        var bodydef=BodyDef()
+        bodydef.position.set(25F,10F)
+        bodydef.type=BodyDef.BodyType.StaticBody
 
-        var buttonstyle=Button.ButtonStyle(tex,tex,tex)
-        var but= Button(buttonstyle)
-        var container= Container(but)
+        var poly=PolygonShape()
+        poly.setAsBox(20F,5F)
 
-        but.setSize(200F,200F)
 
-        but.setFillParent(true)
-        container.setPosition(200F,200F);
+        var fixturedef=FixtureDef()
+        fixturedef.shape=poly
 
-        container.fill()
+        var body=world.createBody(bodydef)
+        body.createFixture(fixturedef)
+
+
+
 
        // container.setSize((tex.getRegion().regionWidth()).toFloat(),tex.region.regionHeight())
         stage= Stage()
 
-        stage.addActor(container)
+       // stage.addActor(container)
         stage.setDebugAll(true)
 
        // var font= BitmapFont(Gdx.files.internal("dsd"));
+        Gdx.input.setInputProcessor(this)
 
     }
 
@@ -82,12 +159,41 @@ class Main : ApplicationAdapter() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
+
+
+        batch.setProjectionMatrix(cam.combined)
         batch.begin()
-        batch.draw(img, 0f, 0f)
+        batch.draw(img, 0f, 0f,5f,5f)
         batch.end()
 
         stage.draw()
         stage.act()
+
+        b2dRenderer.render(world,cam.combined)
+
+        world.step(1/60F,6,4)
+
     }
+
+}
+
+
+class MyContactListener:ContactListener{
+    override fun postSolve(contact: Contact?, impulse: ContactImpulse?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun preSolve(contact: Contact?, oldManifold: Manifold?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun beginContact(contact: Contact?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun endContact(contact: Contact?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
 }
