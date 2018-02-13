c2045bdb3512e91796834fb9073bf3ff77d910b2
Abhishek Aryan <itsabhiaryan@gmail.com>
2018-02-13 13:11:38 +0530

    minorchanges

---
+++ core/src/com/its/two/GdxGame.kt
@@ -0,0 +1,24 @@
+package com.its.two
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/**
+ * Created by Abhishek on 13-02-2018.
+ */
+class GdxGame : ApplicationAdapter(){
+
+
+
+    override fun create() {
+
+    }
+
+    override fun render() {
+
+    }
+
+    override fun resize(width: Int, height: Int) {
+
+    }
+
+}
